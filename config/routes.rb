95ef68943cc20098511f408a5f7f3859c4aa0739
Deniss <den.b@itservicesgroup.co.uk>
2017-07-24 18:47:27 +0100

    Add Relevant routes, authenticate with google

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
