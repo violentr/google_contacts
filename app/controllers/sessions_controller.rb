0b6826270b6998e9859683309ec1d45f765416c2
Deniss <den.b@itservicesgroup.co.uk>
2017-07-24 21:12:51 +0100

    Add Refactor sessions#create with GoogleContactsService object

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,7 @@ class SessionsController < ApplicationController
   def create
     token = request.env['omniauth.auth']['credentials']['token']
     user_email = request.env['omniauth.auth'].info.email
-    uri = URI.parse('https://www.google.com')
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    path = '/m8/feeds/contacts/'+user_email.to_s+'/full?alt=json'
-
-    headers = {'Authorization' => "Bearer #{token}",
-                 'GData-Version' => "3.0"}
-    response = http.get(path, headers)
-    data = JSON.parse(response.body)
-    render html: data
+    contacts = ::GoogleContactsService.new(token, user_email)
+    @data = contacts.list_accounts
   end
 end
