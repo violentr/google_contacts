827c90077578294649cac715dd33499968783678
Deniss <den.b@itservicesgroup.co.uk>
2017-07-24 21:34:06 +0100

    Add Controllerr Spec , returns http success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create
+    return {} unless request.env['omniauth.auth']
     token = request.env['omniauth.auth']['credentials']['token']
     user_email = request.env['omniauth.auth'].info.email
     contacts = ::GoogleContactsService.new(token, user_email)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get 'sessions/create', to: 'sessions#create'
   get '/auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
