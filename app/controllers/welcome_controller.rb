f76a0574694b7f6f2f8ac781db353b565a4f1f5a
Deniss <den.b@itservicesgroup.co.uk>
2017-07-25 16:08:20 +0100

    Add Move logic from sessions controller to welcome
    All necessary actions done to move logic from sessions
    controller to welcome, routes views spec files changed.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,12 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def create
+    return {} unless request.env['omniauth.auth']
+    token = request.env['omniauth.auth']['credentials']['token']
+    user_email = request.env['omniauth.auth'].info.email
+    contacts = ::GoogleContactsService.new(token, user_email)
+    data = contacts.list_contacts
+    @sorted_data = data.sort_by!(&:name).uniq
+  end
 end
