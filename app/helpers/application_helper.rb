397a87ae35989973106f1a594434a00dc6549fde
Deniss <den.b@itservicesgroup.co.uk>
2017-07-25 19:55:19 +0100

    Add Helper to paginate pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
     .map(&:capitalize)
     .join(' ')
   end
+
+  def paginate(collection)
+   per_page = 30
+   number_of_pages = (collection.flatten.size / per_page)
+   pages = (1..number_of_pages).to_a
+   collection = pages.zip(pages)
+   select_tag "pages", options_for_select(collection, 1)
+  end
+
 end
