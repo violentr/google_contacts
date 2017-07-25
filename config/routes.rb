4f6d62e226713e57ef7b2d77024f625041e2cdf3
Deniss <den.b@itservicesgroup.co.uk>
2017-07-25 16:08:20 +0100

    Add Move logic from sessions controller to welcome
    All necessary actions done to move logic from sessions
    controller to welcome, routes views spec files changed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/auth/:provider/callback/', to: 'welcome#create'
   get 'sessions/create', to: 'sessions#create'
-  get '/auth/:provider/callback', to: 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
