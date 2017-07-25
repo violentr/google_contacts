3ac046e4a1e2d1ca295c40bdb156019daa783024
Deniss <den.b@itservicesgroup.co.uk>
2017-07-25 13:05:11 +0100

    Add Output data with contacts sorted by name, alphabeticaly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
     token = request.env['omniauth.auth']['credentials']['token']
     user_email = request.env['omniauth.auth'].info.email
     contacts = ::GoogleContactsService.new(token, user_email)
-    @data = contacts.list_contacts
+    data = contacts.list_contacts
+    @sorted_data = data.sort_by!(&:name)
   end
 end
